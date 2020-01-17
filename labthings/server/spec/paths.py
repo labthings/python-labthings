c4ddb0a0ad8ebcebb9cdfdde4a93e3e6b16e3dd4
jtc42 <jtc9242@gmail.com>
2020-01-17 21:58:10 +0000

    Fixed API Spec structure for parameters

--- labthings/server/spec/paths.py
+++ labthings/server/spec/paths.py
@@ -39,7 +39,8 @@ def argument_to_param(argument, rule, override=None):
     type_, format_ = CONVERTER_MAPPING.get(
         type(rule._converters[argument]), DEFAULT_TYPE
     )
-    param["type"] = type_
+    param["schema"] = {}
+    param["schema"]["type"] = type_
     if format_ is not None:
         param["format"] = format_
     if rule.defaults and argument in rule.defaults:
