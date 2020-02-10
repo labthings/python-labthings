55cee68c1eb884abac993d2e2f5e9399d7f35919
Joel Collins <joel@jtcollins.net>
2020-02-10 17:47:38 +0000

    Started adding missing docstrings

--- labthings/core/tasks/pool.py
+++ labthings/core/tasks/pool.py
@@ -88,11 +88,19 @@ def states():
 
 
 def cleanup_tasks():
+    """
+    Remove all finished tasks from the task list
+    """
     global DEFAULT_TASK_MASTER
     return DEFAULT_TASK_MASTER.cleanup()
 
 
 def remove_task(task_id: str):
+    """Remove a particular task from the task list
+    
+    Arguments:
+        task_id {str} -- ID of the target task
+    """
     global DEFAULT_TASK_MASTER
     return DEFAULT_TASK_MASTER.remove(task_id)
 
@@ -101,6 +109,13 @@ def remove_task(task_id: str):
 
 
 def current_task():
+    """Return the Task instance in which the caller is currently running.
+
+    If this function is called from outside a Task thread, it will return None.
+    
+    Returns:
+        TaskThread -- Currently running Task thread.
+    """
     current_task_thread = threading.current_thread()
     if not isinstance(current_task_thread, TaskThread):
         return None
@@ -108,6 +123,13 @@ def current_task():
 
 
 def update_task_progress(progress: int):
+    """Update the progress of the Task in which the caller is currently running.
+
+    If this function is called from outside a Task thread, it will do nothing.
+    
+    Arguments:
+        progress {int} -- Current progress, in percent (0-100)
+    """
     if current_task():
         current_task().update_progress(progress)
     else:
@@ -115,6 +137,13 @@ def update_task_progress(progress: int):
 
 
 def update_task_data(data: dict):
+    """Update the data of the Task in which the caller is currently running.
+
+    If this function is called from outside a Task thread, it will do nothing.
+    
+    Arguments:
+        data {dict} -- Additional data to merge with the Task data
+    """
     if current_task():
         current_task().update_data(data)
     else:
