a256ed1f5f07d7c1689cb3fd89bcfe6e56337360
Joel Collins <joel@jtcollins.net>
2020-03-08 21:15:56 +0000

    Add description to property builder

--- examples/builder.py
+++ examples/builder.py
@@ -4,14 +4,16 @@ import functools
 
 from labthings.server.quick import create_app
 from labthings.server.view import View
-from labthings.server.decorators import ThingProperty, PropertySchema, use_args
+from labthings.server.decorators import ThingProperty, PropertySchema, use_args, Doc
 
 from labthings.server.types import value_to_field, data_dict_to_schema
 
 from components.pdf_component import PdfComponent
 
 
-def gen_property(property_object, property_name, name: str = None, readonly=False):
+def gen_property(
+    property_object, property_name, name: str = None, readonly=False, description=None
+):
 
     # Create a class name
     if not name:
@@ -57,6 +59,11 @@ def gen_property(property_object, property_name, name: str = None, readonly=Fals
     generated_class = PropertySchema(property_schema)(generated_class)
     generated_class = ThingProperty(generated_class)
 
+    if description:
+        generated_class = Doc(description=description, summary=description)(
+            generated_class
+        )
+
     return generated_class
 
 
@@ -74,8 +81,18 @@ my_component = PdfComponent()
 labthing.add_component(my_component, "org.labthings.example.mycomponent")
 
 # Add routes for the API views we created
-labthing.add_view(gen_property(my_component, "magic_denoise"), "/denoise")
-labthing.add_view(gen_property(my_component, "magic_dictionary"), "/dictionary")
+labthing.add_view(
+    gen_property(my_component, "magic_denoise", description="A magic denoise property"),
+    "/denoise",
+)
+labthing.add_view(
+    gen_property(
+        my_component,
+        "magic_dictionary",
+        description="A big dictionary of little properties",
+    ),
+    "/dictionary",
+)
 
 # Start the app
 if __name__ == "__main__":
