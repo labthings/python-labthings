8389b916c17e1474dd38b29183cfc964c5ba9b69
Joel Collins <joel@jtcollins.net>
2020-03-12 17:15:37 +0000

    Only let eventlet websockets handle connections requesting an upgrade

--- labthings/server/sockets/eventlet.py
+++ labthings/server/sockets/eventlet.py
@@ -4,6 +4,7 @@ from werkzeug.exceptions import NotFound
 from werkzeug.http import parse_cookie
 from werkzeug.wrappers import Request
 from flask import request
+from pprint import pformat
 import logging
 
 from .base import BaseSockets, process_socket_message
@@ -21,22 +22,27 @@ class SocketMiddleware(object):
         request = Request(environ)
         adapter = self.ws.url_map.bind_to_environ(environ)
 
-        logging.debug(environ)
+        logging.debug(pformat(environ))
 
-        try:  # Try matching to a Sockets route
-            handler, values = adapter.match()
-            cookie = None
-            if "HTTP_COOKIE" in environ:
-                cookie = parse_cookie(environ["HTTP_COOKIE"])
+        if environ.get("HTTP_UPGRADE") == "websocket":
+            try:  # Try matching to a Sockets route
+                handler, values = adapter.match()
+                cookie = None
+                if "HTTP_COOKIE" in environ:
+                    cookie = parse_cookie(environ["HTTP_COOKIE"])
 
-            with self.app.app_context():
-                with self.app.request_context(environ):
-                    # add cookie to the request to have correct session handling
-                    request.cookie = cookie
+                with self.app.app_context():
+                    with self.app.request_context(environ):
+                        # add cookie to the request to have correct session handling
+                        request.cookie = cookie
 
-                    websocket.WebSocketWSGI(handler)(environ, start_response, **values)
-                    return []
-        except (NotFound, KeyError) as e:
+                        websocket.WebSocketWSGI(handler)(
+                            environ, start_response, **values
+                        )
+                        return []
+            except (NotFound, KeyError):  # If no socket route found, fall back to WSGI
+                return self.wsgi_app(environ, start_response)
+        else:  # If not upgrading to a websocket
             return self.wsgi_app(environ, start_response)
 
 
