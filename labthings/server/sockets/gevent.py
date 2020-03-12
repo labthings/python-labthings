8e2383f91aed22dd9e8b9ffb45b9b03c868378a9
Joel Collins <joel@jtcollins.net>
2020-03-12 19:07:23 +0000

    Better handle requests without websocket upgrade

--- labthings/server/sockets/gevent.py
+++ labthings/server/sockets/gevent.py
@@ -1,10 +1,7 @@
 """
-Copyright (C) 2013 Kenneth Reitz
-
+Once upon a time, based on flask-websocket; Copyright (C) 2013 Kenneth Reitz
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
@@ -28,21 +25,25 @@ class SocketMiddleware(object):
 
     def __call__(self, environ, start_response):
         adapter = self.ws.url_map.bind_to_environ(environ)
-        try:
-            handler, values = adapter.match()
-            environment = environ["wsgi.websocket"]
-            cookie = None
-            if "HTTP_COOKIE" in environ:
-                cookie = parse_cookie(environ["HTTP_COOKIE"])
 
-            with self.app.app_context():
-                with self.app.request_context(environ):
-                    # add cookie to the request to have correct session handling
-                    request.cookie = cookie
+        if environ.get("HTTP_UPGRADE") == "websocket":
+            try:  # Try matching to a Sockets route
+                handler, values = adapter.match()
+                environment = environ["wsgi.websocket"]
+                cookie = None
+                if "HTTP_COOKIE" in environ:
+                    cookie = parse_cookie(environ["HTTP_COOKIE"])
 
-                    handler(environment, **values)
-                    return []
-        except (NotFound, KeyError):
+                with self.app.app_context():
+                    with self.app.request_context(environ):
+                        # add cookie to the request to have correct session handling
+                        request.cookie = cookie
+
+                        handler(environment, **values)
+                        return []
+            except (NotFound, KeyError):
+                return self.wsgi_app(environ, start_response)
+        else:  # If not upgrading to a websocket
             return self.wsgi_app(environ, start_response)
 
 
@@ -54,6 +55,10 @@ class Sockets(BaseSockets):
 def socket_handler_loop(ws):
     while not ws.closed:
         message = ws.receive()
+        if message is None:
+            break
         response = process_socket_message(message)
-        ws.send(response)
+        if response:
+            logging.info(response)
+            ws.send(response)
         gevent.sleep(0.1)
