85a1cd0736abb9d4d666dfd69ab5bd1776c184de
Joel Collins <joel@jtcollins.net>
2020-03-15 17:07:27 +0000

    Removed module-level monkey patch

--- labthings/server/__init__.py
+++ labthings/server/__init__.py
@@ -1,11 +1,3 @@
 import logging
 
 EXTENSION_NAME = "flask-labthings"
-
-# Monkey patching is bad and should never be done
-# import eventlet
-# eventlet.monkey_patch()
-
-from gevent import monkey
-
-monkey.patch_all()
