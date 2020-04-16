bab208a596ea7a2643591b4214adccc34072fde4
Joel Collins <joel@jtcollins.net>
2020-04-16 12:08:32 +0100

    Added LabThing fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,13 @@ import os
 from flask import Flask
 from apispec import APISpec
 from apispec.ext.marshmallow import MarshmallowPlugin
+from labthings.server.labthing import LabThing
+from labthings.server.view import View
 
 
 @pytest.fixture
 def view_cls():
-    class ViewClass:
+    class ViewClass(View):
         def get(self):
             pass
 
@@ -46,6 +48,13 @@ def app(request):
     # bind the test life with the context through the
     request.addfinalizer(ctx.pop)
     return app
+
+
+@pytest.fixture
+def thing(app):
+    thing = LabThing(app)
+    with app.app_context():
+        return thing
 
 
 @pytest.fixture()
@@ -90,3 +99,8 @@ def client(app):
 @pytest.fixture
 def static_path(app):
     return os.path.join(os.path.dirname(__file__), "static")
+
+
+@pytest.fixture
+def schemas_path(app):
+    return os.path.join(os.path.dirname(__file__), "schemas")
