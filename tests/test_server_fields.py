6b29de8672d3ff782bcc9607ee72baf31f2377ba
Joel Collins <jtc9242@gmail.com>
2020-05-02 18:12:29 +0100

    Test b64 decoding

--- tests/test_server_fields.py
+++ tests/test_server_fields.py
@@ -2,12 +2,13 @@ from labthings.server import schema
 from labthings.server import fields
 
 from marshmallow import ValidationError
+from base64 import b64encode
 import pickle
 
 import pytest
 
 
-def test_bytes_in_schema():
+def test_bytes_encode():
     test_schema = schema.Schema.from_dict({"b": fields.Bytes()})()
 
     obj = type("obj", (object,), {"b": pickle.dumps(object())})
@@ -15,6 +16,23 @@ def test_bytes_in_schema():
     assert test_schema.dump(obj) == {
         "b": b"\x80\x04\x95\x1a\x00\x00\x00\x00\x00\x00\x00\x8c\x08builtins\x94\x8c\x06object\x94\x93\x94)\x81\x94.",
     }
+
+
+def test_bytes_decode():
+    test_schema = schema.Schema.from_dict({"b": fields.Bytes()})()
+
+    data = {"b": pickle.dumps(object())}
+
+    assert test_schema.load(data) == data
+
+
+def test_bytes_decode_string():
+    test_schema = schema.Schema.from_dict({"b": fields.Bytes()})()
+
+    data = {"b": pickle.dumps(object())}
+    encoded_data = {"b": b64encode(data["b"]).decode()}
+
+    assert test_schema.load(encoded_data) == data
 
 
 def test_bytes_validate():
