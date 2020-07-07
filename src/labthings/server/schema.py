0582e772bf25ea971773758a7e7e401f957b6921
Joel Collins <joel@jtcollins.net>
2020-07-07 10:47:18 +0100

    Re-added compatibility import

--- src/labthings/server/schema.py
+++ src/labthings/server/schema.py
@@ -1,1 +1,21 @@
+from ..schema import (
+    Schema,
+    FieldSchema,
+    TaskSchema,
+    ExtensionSchema,
+    ActionSchema,
+    build_action_schema,
+    pre_load,
+    pre_dump,
+)
 
+__all__ = [
+    "Schema",
+    "FieldSchema",
+    "TaskSchema",
+    "ExtensionSchema",
+    "ActionSchema",
+    "build_action_schema",
+    "pre_load",
+    "pre_dump",
+]
