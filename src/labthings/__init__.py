070bfcd78692ab82117854faf3843c6021bd3ac4
Joel Collins <joel@jtcollins.net>
2020-07-19 11:53:45 +0100

    Added create_app to top level import

--- src/labthings/__init__.py
+++ src/labthings/__init__.py
@@ -1,14 +1,15 @@
 from .labthing import LabThing
 from .wsgi import Server
-from . import views, sync, fields, schema, semantics, quick
+from .quick import create_app
+from . import views, sync, fields, schema, semantics
 
 __all__ = [
     "LabThing",
     "Server",
+    "create_app",
     "views",
     "sync",
     "fields",
     "schema",
     "semantics",
-    "quick",
 ]
