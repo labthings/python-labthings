5fee550a0c15ba59a2eee1221307945a497f956f
Joel Collins <joel@jtcollins.net>
2020-07-19 12:24:31 +0100

    Flattened imports

--- src/labthings/__init__.py
+++ src/labthings/__init__.py
@@ -8,25 +8,23 @@ from .quick import create_app
 from .wsgi import Server
 
 # Functions to speed up finding global objects
-from .find import (
-    current_labthing,
-    registered_extensions,
-    registered_components,
-    find_extension,
-    find_component,
-)
+from .find import current_labthing
+from .find import registered_extensions
+from .find import registered_components
+from .find import find_extension
+from .find import find_component
 
 # Synchronisation classes
-from .sync import StrictLock, CompositeLock, ClientEvent
+from .sync import StrictLock
+from .sync import CompositeLock
+from .sync import ClientEvent
 
 # Task management functions
-from .tasks import (
-    current_task,
-    current_task_stopped,
-    update_task_progress,
-    update_task_data,
-    TaskKillException,
-)
+from .tasks import current_task
+from .tasks import current_task_stopped
+from .tasks import update_task_progress
+from .tasks import update_task_data
+from .tasks import TaskKillException
 
 # Submodules
 from . import extensions
