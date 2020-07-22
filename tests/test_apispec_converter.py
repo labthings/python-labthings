1bf0c276a745c1aa7457290f601a3410650fb383
Joel Collins <joel@jtcollins.net>
2020-07-22 15:12:42 +0100

    Improved ExtendedOpenAPIConverter coverage

---
+++ tests/test_apispec_converter.py
@@ -0,0 +1,21 @@
+from labthings.apispec.converter import ExtendedOpenAPIConverter
+
+from labthings import fields
+
+
+class TestField(fields.Field):
+    def _jsonschema_type_mapping(self):
+        """ """
+        return {"type": "string"}
+
+
+def test_jsonschema_type_mapping(spec):
+    converter = ExtendedOpenAPIConverter("3.0.2", lambda _: None, spec)
+
+    assert converter.jsonschema_type_mapping(TestField()) == {"type": "string"}
+
+
+def test_jsonschema_type_mapping_missing(spec):
+    converter = ExtendedOpenAPIConverter("3.0.2", lambda _: None, spec)
+
+    assert converter.jsonschema_type_mapping(fields.Field()) == {}
