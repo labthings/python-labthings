2605d7ccd7135d0091f986885e7cfe64b59845fb
Joel Collins <joel@jtcollins.net>
2020-07-29 17:10:16 +0100

    Add crude example of watching property for changes

--- examples/simple_thing.py
+++ examples/simple_thing.py
@@ -44,6 +44,17 @@ class DenoiseProperty(PropertyView):
 
         return my_component.integration_time
 
+    @op.observeproperty
+    def websocket(self, ws):
+        # Find our attached component
+        my_component = find_component("org.labthings.example.mycomponent")
+        initial_value = None
+        while not ws.closed:
+            time.sleep(1)
+            if my_component.integration_time != initial_value:
+                ws.send(encode_json(my_component.integration_time))
+                initial_value = my_component.integration_time
+
 
 """
 Create a view to quickly get some noisy data, and register is as a Thing property
@@ -68,7 +79,6 @@ class QuickDataProperty(PropertyView):
         my_component = find_component("org.labthings.example.mycomponent")
         while not ws.closed:
             ws.send(encode_json(my_component.data))
-            # time.sleep(0.5)
 
 
 """
